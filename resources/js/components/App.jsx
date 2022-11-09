fb8bece2f36db37938a2811355963e9f4383a0f4
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2022-11-09 18:30:16 +0200

    login

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -5,11 +5,9 @@ function App() {
     return (
 
             <div >
-<<<<<<< HEAD
+
             <Login/>
-=======
-         <Example/>
->>>>>>> 989bcb53c5b6b59eab5286e5009966b5912b9145
+
             </div>
     );
 }
