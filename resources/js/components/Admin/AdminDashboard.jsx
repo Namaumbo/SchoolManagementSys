ebe17a4141b02c8c956f64711c1b4750ac77413d
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2023-01-16 13:41:14 +0200

    dashboard

--- resources/js/components/Admin/AdminDashboard.jsx
+++ resources/js/components/Admin/AdminDashboard.jsx
@@ -1,9 +1,8 @@
-import React, {useEffect} from "react";
-import "../../../css/admindash.css"
+import React from "react";
+import "./admindash.css"
 import {userState} from "../User/userState"
 import {userDetails} from "@/components/recoil_states/userdetails";
 import {useRecoilState} from "recoil";
-import TotalAvatars from "@/components/People";
 import teachers from '../../../assets/teacher.png'
 import UserInfo from "@/components/UserInfo";
 import users from '../../../assets/users.png'
@@ -12,17 +11,18 @@ import absentUser from '../../../assets/absentUser.png'
 import DatePicker from "@/components/DatePicker";
 import {DataGrid} from "@mui/x-data-grid";
 import {Navigate} from "react-router-dom";
+import {Button} from "@mui/material";
 
 
 export default function AdminDashboard() {
     let [{loggedIn, role}] = useRecoilState(userState)
-    const [userInfo , setUserInfo] = useRecoilState(userDetails)
+    const [userInfo, setUserInfo] = useRecoilState(userDetails)
     const columns = [
         {field: 'id', headerName: 'ID', width: 50},
         {field: 'firstname', headerName: 'First name', width: 130},
         {field: 'surname', headerName: 'Last name', width: 130},
         {field: 'Sex', headerName: 'Sex', type: 'String', width: 10},
-        {field: 'email',headerName: 'Email',type: 'String',width: 180},
+        {field: 'email', headerName: 'Email', type: 'String', width: 180},
         {field: 'role_name', headerName: 'Role', width: 100}
     ];
     const rows = userInfo;
@@ -93,7 +93,7 @@ export default function AdminDashboard() {
                             </div>
                         </div>
                     </div>
-                    <br />
+                    <br/>
                     <div className="container text-center" style={{marginTop: "5px"}}>
                         <div style={{display: "flex", justifyContent: "space-between"}}>
                                  <span className="headings">
@@ -115,118 +115,9 @@ export default function AdminDashboard() {
                                 </div>
                                 <div style={{display: "flex", justifyContent: "space-between"}}>
                                  <span className="headings">
-                                     <br />
+                                     <br/>
                                  <b>STATISTICS</b>
                              </span>
-                                </div>
-                                <hr/>
-                                <div className="col-11">
-                                    <div className="container text-center">
-                                        <div className="row">
-                                            <div className="col-sm-5 col-md-6">
-                                                <div className="graph">
-                                                    {/*<Chart student="Male" percentages="56"/>*/}
-                                                </div>
-                                            </div>
-                                            <div className="col-sm-5 offset-sm-2 col-md-6 offset-md-0">
-                                                <div className="graph">
-                                                    {/*<Chart*/}
-                                                    {/*    student="Females" percentages="44"/>*/}
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="col-5" style={{marginLeft:"10px"}}>
-                                <div className="card">
-                                    <DatePicker/>
-                                </div>
-                                <br />
-                                <span className="headings">
-                                 <b>EMPLOYEES</b>
-                             </span>
-                                <br/>
-
-                                <div className="card">
-                                    <div className="people">
-                                        <TotalAvatars/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-    else if (loggedIn && role === 'h-teacher') {
-    return (
-        <div>
-            <div>
-                <div style={{maxWidth: "70%"}}>
-
-                    <div className="container text-center">
-                        <div className="row">
-                            <div className="col">
-                                <div style={{display: "flex",marginLeft:"45%"}}>
-                                    <div className="col-6">
-                                        <div className="cards">
-                                            <div className="card-header bg-transparent border-success">
-                                                <span className="heading">Members Available</span>
-                                                <hr/>
-                                            </div>
-                                            <span style={{display: "flex", justifyContent: "space-around"}}>
-                                                          <span><h4>3/{rows.length} </h4></span>
-                                                    <span><img src={teachers} alt="users" className="usersCard"/></span>
-                                                      </span>
-                                        </div>
-                                    </div>
-                                    <div className="col-6">
-                                        <div className="cards">
-                                            <div className="card-header bg-transparent border-success">
-
-                                                <span className="heading">Students Available</span></div>
-                                            <hr/>
-                                            <span style={{display: "flex", justifyContent: "space-around"}}>
-                                                          <span><h4>356</h4></span>
-                                                     <span>
-                                                         <img src={students} alt="users"
-                                                                className="usersCard"/></span>
-                                                     </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <br/>
-                    <div className="container text-center" style={{marginTop: "5px"}}>
-                        <div style={{display: "flex", justifyContent: "space-between"}}>
-                                                 <span className="headings">
-                                                 <b>STUDENTS</b>
-                                             </span>
-                        </div>
-                    </div>
-                    <div className="container text-center">
-                        <div style={{display: "flex", justifyContent: "space-between"}}>
-                            <div className="col-11">
-                                <div style={{height: 300, width: '100%'}}>
-                                    <DataGrid
-                                        rows={rows}
-                                        columns={columns}
-                                        pageSize={5}
-                                        rowsPerPageOptions={[6]}
-                                        checkboxSelection
-                                    />
-                                </div>
-                                <div style={{display: "flex", justifyContent: "space-between"}}>
-                                                 <span className="headings">
-                                                     <br/>
-                                                 <b>STATISTICS</b>
-                                             </span>
                                 </div>
                                 <hr/>
                                 <div className="col-11">
@@ -255,12 +146,45 @@ export default function AdminDashboard() {
                                 </div>
                                 <br/>
                                 <span className="headings">
-                                                 <b>UPCOMING CLASSES</b>
-                                             </span>
+                                 <b>EMPLOYEES</b>
+                             </span>
                                 <br/>
                                 <div className="card">
                                     <div className="people">
+                                        <ul>
 
+                                            <li className="friends">
+                                                <div className='profile-holder'>
+                                                    A
+                                                </div>
+                                                <span className="avatarText">Sam Tinted</span>
+                                            </li>
+                                            <li className="friends">
+                                                <div className='profile-holder'>
+                                                    A
+                                                </div>
+                                                <span className="avatarText">Alex Yohah</span>
+                                            </li>
+                                            <li className="friends">
+                                                <div className='profile-holder'>
+                                                    A
+                                                </div>
+                                                <span className="avatarText">Enock Chokha</span>
+                                            </li>
+                                            <li className="friends">
+                                                <div className='profile-holder'>
+                                                    A
+                                                </div>
+                                                <span className="avatarText">Rabbina Chokha</span>
+                                            </li>
+                                            <li className="friends">
+                                                <div className='profile-holder'>
+                                                    A
+                                                </div>
+                                                <span className="avatarText">Daelo Chokha</span>
+                                            </li>
+                                        </ul>
+                                        <Button variant='outlined'>Show more</Button>
                                     </div>
                                 </div>
                             </div>
@@ -268,10 +192,116 @@ export default function AdminDashboard() {
                     </div>
                 </div>
             </div>
-        </div>
-    );
-    }
-    else if (loggedIn && role === 'teacher') {
+        );
+    } else if (loggedIn && role === 'h-teacher') {
+        return (
+            <div>
+                <div>
+                    <div style={{maxWidth: "70%"}}>
+
+                        <div className="container text-center">
+                            <div className="row">
+                                <div className="col">
+                                    <div style={{display: "flex", marginLeft: "45%"}}>
+                                        <div className="col-6">
+                                            <div className="cards">
+                                                <div className="card-header bg-transparent border-success">
+                                                    <span className="heading">Members Available</span>
+                                                    <hr/>
+                                                </div>
+                                                <span style={{display: "flex", justifyContent: "space-around"}}>
+                                                          <span><h4>3/{rows.length} </h4></span>
+                                                    <span><img src={teachers} alt="users" className="usersCard"/></span>
+                                                      </span>
+                                            </div>
+                                        </div>
+                                        <div className="col-6">
+                                            <div className="cards">
+                                                <div className="card-header bg-transparent border-success">
+
+                                                    <span className="heading">Students Available</span></div>
+                                                <hr/>
+                                                <span style={{display: "flex", justifyContent: "space-around"}}>
+                                                          <span><h4>356</h4></span>
+                                                     <span>
+                                                         <img src={students} alt="users"
+                                                              className="usersCard"/></span>
+                                                     </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <br/>
+                        <div className="container text-center" style={{marginTop: "5px"}}>
+                            <div style={{display: "flex", justifyContent: "space-between"}}>
+                                                 <span className="headings">
+                                                 <b>STUDENTS</b>
+                                             </span>
+                            </div>
+                        </div>
+                        <div className="container text-center">
+                            <div style={{display: "flex", justifyContent: "space-between"}}>
+                                <div className="col-11">
+                                    <div style={{height: 300, width: '100%'}}>
+                                        <DataGrid
+                                            rows={rows}
+                                            columns={columns}
+                                            pageSize={5}
+                                            rowsPerPageOptions={[6]}
+                                            checkboxSelection
+                                        />
+                                    </div>
+                                    <div style={{display: "flex", justifyContent: "space-between"}}>
+                                                 <span className="headings">
+                                                     <br/>
+                                                 <b>STATISTICS</b>
+                                             </span>
+                                    </div>
+                                    <hr/>
+                                    <div className="col-11">
+                                        <div className="container text-center">
+                                            <div className="row">
+                                                <div className="col-sm-5 col-md-6">
+                                                    <div className="graph">
+                                                        {/*<Chart student="Male" percentages="56"/>*/}
+                                                    </div>
+                                                </div>
+                                                <div className="col-sm-5 offset-sm-2 col-md-6 offset-md-0">
+                                                    <div className="graph">
+                                                        {/*<Chart*/}
+                                                        {/*    student="Females" percentages="44"/>*/}
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-5" style={{marginLeft: "10px"}}>
+                                    <div className="card">
+                                        <DatePicker/>
+                                    </div>
+                                    <br/>
+                                    <span className="headings">
+                                                 <b>UPCOMING CLASSES</b>
+                                             </span>
+                                    <br/>
+                                    <div className="card">
+                                        <div className="people">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    } else if (loggedIn && role === 'teacher') {
         return (
 
             <div>
@@ -280,7 +310,7 @@ export default function AdminDashboard() {
                     <div className="container text-center">
                         <div className="row">
                             <div className="col">
-                                <div style={{display: "flex",marginLeft:"45%"}}>
+                                <div style={{display: "flex", marginLeft: "45%"}}>
                                     <div className="col-6">
                                         <div className="cards">
                                             <div className="card-header bg-transparent border-success">
