ce6f34c07c0f3293f0745394dea5b2e979019a8a
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2023-01-25 23:17:29 +0200

    dashboard

--- resources/js/components/Admin/AdminDashboard.jsx
+++ resources/js/components/Admin/AdminDashboard.jsx
@@ -128,6 +128,37 @@ export default function AdminDashboard() {
                             </div>
                         </div>
                     </div>
+                    <div>
+                        <table className="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td colSpan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
 
                 <div className='scores'>
@@ -143,6 +174,8 @@ export default function AdminDashboard() {
                         <div className='chartItem'><Chart/></div>
                     </div>
                 </div>
+
+
             </div>
         </>
 
