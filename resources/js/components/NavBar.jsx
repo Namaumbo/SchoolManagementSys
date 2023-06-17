c14d4ac740fcb29498c7db5c23be25e2d675de25
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2023-06-17 22:58:28 +0200

    rf

--- resources/js/components/NavBar.jsx
+++ resources/js/components/NavBar.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import "./navbar.css";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { FiBell, FiUser } from "react-icons/fi";
 
 export default function NavBar() {
     return (
-        <div className="navbar-wrapper">
-            <div className="nav-items">
-                <div className="logo">Secondary School</div>
-                <div className="profile-vitals">
-                    <div className="user-detail">
-                        <span className="profile-name">
-                            Admin <span> </span> | <span> </span>
-                            <Link to="/logout">
-                                <button className="btn btn-primary btn-sm">
-                                    Logout
-                                </button>
-                            </Link>
-                        </span>
-                    </div>
-                </div>
+        <div className="nav-items">
+            <div className="logo">Secondary School</div>
+            <div className="user-detail">
+                <span className="profile-name">
+                    Admin <span> </span> | <span> </span>
+                    <Link to="/logout">
+                        <button className="btn btn-primary btn-sm">
+                            Logout
+                        </button>
+                    </Link>
+                </span>
             </div>
         </div>
     );
