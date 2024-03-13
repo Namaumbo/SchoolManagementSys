73517073bdefffd07349f644b747c18978e54470
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2024-03-13 23:14:19 +0200

    rf

--- resources/services/SubjectService.js
+++ resources/services/SubjectService.js
@@ -1,20 +1,36 @@
 import HttpService from "./HttpService";
 import axios from "axios";
-
+import utils from "../GlobalUtils/utils";
 
 class SubjectService {
-    constructor() {}
+    constructor() {
+        this.headers = HttpService.setHeaders();
+    }
     // TODO: this url prefix should be coming from  freezed object``
     urlPrefix = "http://127.0.0.1:8000/api";
-    response = null;
-
-    headers = HttpService.setHeaders();
-
+    availableSubjectNames = [
+        "ENGLISH",
+        "MATHEMATICS",
+        "AGRICULTURE",
+        "BIOLOGY",
+        "PHYSICS",
+        "CHEMISTRY",
+        "COMPUTER STUDIES",
+        "CHICHEWA",
+        "BIBLE KNOWLEDGE",
+        "EXPRESSIVE ARTS",
+        "LIFE SKILLS",
+        "OTHER",
+    ];
     /**
      * Makes an HTTP POST request to create a new subject.
+     * Makes an HTTP POST request to create a new subject.
+     * @param {Object} subject - The details of the subject to be created.
      * @param {Object} subject - The details of the subject to be created.
      * @returns {Promise} - A promise that resolves with the response data or rejects with an error object.
+     * @returns {Promise} - A promise that resolves with the response data or rejects with an error object.
      */
+
     async addSubject(subject) {
         try {
             const wordNormalizer = utils.wordNormalizer(subject);
@@ -49,9 +65,9 @@ class SubjectService {
             });
             return response.data;
         } catch (error) {
-            return error;
+            console.log(error);
+            throw new Error("Failed to get subjects", error);
         }
     }
 }
-
 export default new SubjectService();
